0bc944054f2547f36241e9aa771b67e31b4c3365
Swanandi-28 <swanandi.0865@gmail.com>
2026-07-03 22:45:20 +0530

    Login Route

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,54 @@
+import {NextResponse} from "next/server";
+import client from "@/lib/mongodb";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export async function POST(request: Request){
+
+    const {email, password}= await request.json();
+
+    if(!email || !password){
+        return NextResponse.json({ message: "All fields are required"}, { status:400});
+    }
+
+    await client.connect();
+
+    const db=client.db("auth-app");
+    const usersCollection=db.collection("users");
+
+    const user= await usersCollection.findOne({email});
+
+    if(!user){
+        return NextResponse.json({ message: "User not found"}, { status:404});
+    }
+
+    const isMatch = await bcrypt.compare( password, user.password);
+
+    if (!isMatch) {
+        return NextResponse.json(
+            {
+                message: "Invalid password."
+            },
+            {
+                status: 401
+            }
+        );
+    }
+
+    const token = jwt.sign(
+    { 
+        email:user.email,
+    },
+    process.env.JWT_SECRET!,
+    {
+        expiresIn: "1d",
+    }
+    );
+
+    return NextResponse.json({
+        message: "Login Successful!",
+        token,
+    });
+
+
+}
